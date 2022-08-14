083205f0a28d57c1ea628fd5b8537e52c5d0820c
Rodrigo Morán <rodrigobq18@gmail.com>
2022-08-13 22:17:32 -0400

    Starter Code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import WinClick from './WinClick'
+
+class App extends Component{
+  render(){
+    return(
+      <WinClick/>
+    )
+  }
+}
+
+export default App
